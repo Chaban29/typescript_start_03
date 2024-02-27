9fd01a84f265ddd9faff9b49aec357b4e3a8e4a6
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-27 14:06:19 +0200

    learn types, interfaces

--- ts/index.js
+++ ts/index.js
@@ -25,3 +25,33 @@ var greetingUser = function (greet, userName) {
     return "".concat(greet, " ").concat(userName);
 };
 console.log(greetingUser('Hello', 'Roman'));
+var myDog = {
+    petName: 'Julia',
+    petLegs: 4,
+    petColor: 'Black&White',
+    petIsMale: false,
+};
+var strings = ['1', '2', '3', '4', '5'];
+var numbers = [1, 2, 3, 4, 5];
+var obj = [
+    {
+        name: 100,
+        age: 23,
+    },
+];
+var age = 23;
+var isHasDog = false;
+var universityAbbreviation = 'KNU';
+var myRealAge = 23;
+isHasDog = true;
+var myBrotherName = 'Bogdan';
+var numberSome = '1010212011';
+numberSome = 10010101021211;
+var u = 100;
+u = '100';
+var y = 1;
+console.log(y);
+y = '2';
+console.log(y);
+y = true;
+console.log(y);
--- ts/index.ts
+++ ts/index.ts
@@ -19,7 +19,7 @@ type Person = {
   name: string;
   age: number;
   isMarried?: boolean | string;
-  favoriteColor?: string,
+  favoriteColor?: string;
 };
 
 const newPerson: Person = {
@@ -30,25 +30,83 @@ const newPerson: Person = {
 
 newPerson.favoriteColor = 'red';
 
-
 interface Person_2 {
-  name: string,
-  age: number,
-  hasChildren?: boolean,
+  name: string;
+  age: number;
+  hasChildren?: boolean;
 }
-
 
 const currentUser: Person_2 = {
   name: 'John',
   age: 40,
   hasChildren: true,
-}
+};
 
-
-const greetingUser = (greet: string, userName: string): string =>  {
-  return `${greet} ${userName}`
-}
-
+const greetingUser = (greet: string, userName: string): string => {
+  return `${greet} ${userName}`;
+};
 
 console.log(greetingUser('Hello', 'Roman'));
 
+interface homePet {
+  petName: string;
+  petLegs?: number;
+  petColor: string;
+  petIsMale?: boolean;
+}
+
+const myDog: homePet = {
+  petName: 'Julia',
+  petLegs: 4,
+  petColor: 'Black&White',
+  petIsMale: false,
+};
+
+const strings: string[] = ['1', '2', '3', '4', '5'];
+
+const numbers: Array<number> = [1, 2, 3, 4, 5];
+
+type UserName = string | number;
+
+type SomeObj = {
+  name: UserName;
+  age: number;
+  getMoreInfo?: (numbers: []) => Array<number>;
+};
+
+const obj: Array<SomeObj> = [
+  {
+    name: 100,
+    age: 23,
+  },
+];
+
+let age: number | string = 23;
+
+let isHasDog: boolean = false;
+let universityAbbreviation: string = 'KNU';
+let myRealAge: number | string = 23;
+
+isHasDog = true;
+
+let myBrotherName: string = 'Bogdan';
+
+let numberSome: any = '1010212011';
+
+numberSome = 10010101021211;
+
+let u: any = 100;
+u = '100';
+
+
+let y: unknown = 1;
+console.log(y);
+y = '2';
+console.log(y);
+y = true;
+console.log(y);
+
+
+let not: string;
+
+not = 'hello';
